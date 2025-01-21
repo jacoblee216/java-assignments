4c9d80341ccac810d5fb0aaba22d198f492ef515
Jacob Lee <jacoblee242006@gmail.com>
2025-01-21 16:28:02 -0500

    final

--- assignment1/src/main/java/assignment1/Main.java
+++ assignment1/src/main/java/assignment1/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public static double footToMeter(double foot) {
         return foot * 0.305;
     }
-    
+
     public static double meterToFoot(double meter) {
         return meter * 3.279;
     }
+    
     public static void main(String[] args) {
         System.out.printf("Feet%10s%20s%10s", "Meters", "Feet", "Meters");
         System.out.println();
